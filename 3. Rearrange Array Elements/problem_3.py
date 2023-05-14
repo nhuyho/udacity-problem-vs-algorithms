ecf73bad9d903b0201fdc4f87f45bc39f08e303b
nhu <chriss.anne25@gmail.com>
2023-05-14 18:17:00 +0700

    update explanation 4

--- 3. Rearrange Array Elements/problem_3.py
+++ 3. Rearrange Array Elements/problem_3.py
@@ -1,123 +1,107 @@
-'''
-Problem 3: Rearrange Array Elements
-'''
+# Would need implementation of sorting algorithm
+# Requirement: O(nlog(n)) efficiency -> use MergeSort or QuickSort
 
-def reverse_merge(left: list, right: list) -> list:
 
-    #Merge sorted arrays for merge sort in reverse order
+def mergesort(items):
+    # Base case, a list of 0 or 1 items is already sorted
+    if len(items) <= 1:
+        return items
 
+    # Otherwise, find the midpoint and split the list
+    mid = len(items)//2
+    left = items[0:mid]
+    right = items[mid:]
+
+    # Call mergesort recursively with the left and right half
+    left = mergesort(left)
+    right = mergesort(right)
+
+    # Merge our two halves and return
+    return merge(left, right)
+
+
+def merge(left, right):
+    # Given two ordered lists, merge them together in order,
+    # returning the merged list.
     merged = []
-    right_ix = 0
-    left_ix = 0
+    left_index = 0
+    right_index = 0
 
-    while left_ix < len(left) and right_ix < len(right):
-        if left[left_ix] > right[right_ix]:
-            merged.append(left[left_ix])
-            left_ix += 1
+    # Iterate through the ordered lists until you reach the end of one list
+    # Modification to the code compared to the example - numbers to be sorted in descending order
+    while left_index < len(left) and right_index < len(right):
+        if left[left_index] < right[right_index]:
+            merged.append(right[right_index])
+            right_index += 1
         else:
-            merged.append(right[right_ix])
-            right_ix += 1
+            merged.append(left[left_index])
+            left_index += 1
 
-    merged += left[left_ix:]
-    merged += right[right_ix:]
+    # Add the remaining values from either the left or right list
+    merged += left[left_index:]
+    merged += right[right_index:]
 
     return merged
 
-def merge_sort(l: list) -> list:
 
-    ''' Basic merge sort, in reverse order
-
-    Args:
-       l(list): Unsorted array
-    Returns:
-       l(list): Sorted array
-    '''
-
-    if len(l) <= 1:
-        return l
-
-    # Find mid-point and split array
-
-    mid = len(l) // 2
-    left = merge_sort(l[:mid])
-    right = merge_sort(l[mid:])
-
-    # Return merged and sorted array
-
-    return reverse_merge(left, right)
-
-def rearrange_digits(input_list: list) -> tuple:
-
-
+def rearrange_digits(input_list):
     """
     Rearrange Array Elements so as to form two number such that their sum is maximum.
 
     Args:
        input_list(list): Input List
     Returns:
-       list: Two maximum sums
+       (int),(int): Two maximum sums
     """
 
-    # Test that list elements are digits
+    # If the input list doesn't contain anything, return empty array
+    if len(input_list) == 0:
+        return []
 
-    assert input_list, 'Must pass a non-empty array of digits'
-    assert all(isinstance(i, int) for i in input_list), 'Must pass an array of digits'
-    assert all(i<10 and i >= 0 for i in input_list), 'Must pass an array of digits'
-
-    # If inpit is a single digit, return that digit
-
+    # If the input list has one number, then the max sum is that number and zero
     if len(input_list) == 1:
-        return input_list[0], None
+        return [input_list[0], 0]
 
-    # Empty lists to hold final digits
+    # Get the ordered list via merge sort
+    ordered_list = mergesort(input_list)
 
-    out1 = []
-    out2 = []
-    
-    # First sort the list
+    # Get every other number in the sorted list
+    left_number_array = ordered_list[0::2]
+    right_number_array = ordered_list[1::2]
 
-    l = merge_sort(input_list)
+    # Build the return values
+    left_number_string = ''
+    for number in left_number_array:
+        left_number_string += str(number)
 
-    # Build two integers one at a time, starting with the largest digits
+    right_number_string = ''
+    for number in right_number_array:
+        right_number_string += str(number)
 
-    for i, el in enumerate(l):
-        if i % 2 == 0:
-            out1.append(el)
-        else:
-            out2.append(el)
-
-    # Convert lists of digits into integers
-
-    out1 = int(''.join([str(i) for i in out1]))
-    out2 = int(''.join([str(i) for i in out2]))
-
-    return out1, out2
+    return [int(left_number_string), int(right_number_string)]
 
 
-if __name__ == '__main__':
-    test_list = [1, 2, 3, 4, 5]
+def test_function(test_case):
+    output = rearrange_digits(test_case[0])
+    solution = test_case[1]
+    if sum(output) == sum(solution):
+        print("Pass")
+    else:
+        print("Fail")
 
-    # Test proper merge behavior
-    assert reverse_merge([1], [2]) == [2, 1]
-    assert reverse_merge([3, 1], [4, 2]) == [4, 3, 2, 1]
 
-    # Test proper sort behavior
-    assert merge_sort(test_list) == [5, 4, 3, 2, 1]
+# Test cases
 
-    # Should print 531, 42
-    print(rearrange_digits([1, 2, 3, 4, 5]))
+test_function([[1, 2, 3, 4, 5], [542, 31]])
 
-    # Should print 964, 852
-    print(rearrange_digits([4, 6, 2, 5, 9, 8]))
+test_function([[4, 6, 2, 5, 9, 8], [964, 852]])
 
-    # Should print 1000, 100
-    print(rearrange_digits([0, 0, 1, 1, 0, 0, 0]))
+test_function([[1, 4], [4, 1]])
 
-    # Should return 0, 0
-    print(rearrange_digits([0, 0, 0, 0, 0, 0, 0]))
+test_function([[1, 4, 2], [41, 2]])
 
-    # Single digit, should print 5, None
-    print(rearrange_digits([5]))
+# Edge Case 1: No inputs
+test_function([[], []])
 
-    # Empty list, should raise an error
-    print(rearrange_digits([]))
+# Edge Case 2: 1 number input
+test_function([[1], [1, 0]])
